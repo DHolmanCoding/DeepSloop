47d701876e76fa80136d848966f9b0cf800a675b
holmando@oregonstate.edu <Codingandmorecoding89>
2018-09-05 11:29:29 -0700

    Updated model saving procedures, documentation, and file naming. Also added in rune time information to provide unique date identifiers.

--- Code/DeepSloop_Model.py
+++ Code/DeepSloop_Model.py
@@ -7,16 +7,21 @@ ultimately not included in the final model(s).
 # Dependencies
 #
 
+import time
+from datetime import datetime
+
 from keras.models import Sequential
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.layers import Bidirectional, CuDNNLSTM
-from keras.layers import Dropout, Dense
+from keras.layers import Dropout, Dense, Flatten
 import keras as keras
 from keras import callbacks
 
 from keras import optimizers
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+import tensorflow as tf
 
 import numpy as np
 from numpy import array
@@ -26,6 +31,7 @@ import Deep_Sloop_Utils as DSU
 import matplotlib.pyplot as plt
 
 import os
+
 
 #
 # Main
@@ -37,27 +43,26 @@ bases = "ACUG"
 
 
 def Deep_Sloop_Model(repository_path=r"C:/Users/Douglas/PycharmProjects/Deep_Sloop/Datasets/Filtered_90_70_Sloops_Loop_3_22_Seg_20_150_FLOL_split_datasets_010_100mult",
-                     Tr_data_type='R_NS_RNS',
-                     batch_size=16, epochs=32,
+                     Tr_data_type='NS_RNS',
                      Convolve=False,
                      cv_filters=128, cv_kernel_size=3, cv_strides=1,
                      mp_pool_size=2, mp_strides=2,
-                     mode1_size=128,
+                     lstm_size=128,
+                     batch_size=16, epochs=32,
                      custom_expt_name="",
                      custom_expt_var="",
                      custom_expt_value=""
                      ):
     """
-    This is a Convolutional, BiLSTM designed to train on sloops.
+    This is a BiLSTM(with optional convolution) designed to train on sloops.
 
     Arguments:
     repository_path -- A string representing the FULL path to the directory of the data repository you wish to use
                        Example: C:/......../filtered_sloops_split_datasets_0
     Tr_data_type -- A string representing the type of data you would like to train on for your deep sloop model
                     types of data:
-                    R_NS - reversed, negated and scrambled
-                    M_R_NS - mutated, reversed, negated and scrambled
-
+                    NS - negated and scrambled
+                    M_NS - mutated, negated and scrambled
 
     # Convolutional Layers
     filters -- An integer representing the dimensionality of the output space (i.e. the number of output filters in the convolution).
@@ -76,6 +81,8 @@ def Deep_Sloop_Model(repository_path=r"C:/Users/Douglas/PycharmProjects/Deep_Slo
     batch_size -- An integer representing the number of sloops you will run through before back propogation
     epochs -- An integer representing the number of times you will pass over your entire dataset
     """
+    start_time = time.time()
+
     fasta_file_name = repository_path.split('Datasets/')[-1].split('split_datasets_')[0]
     Tr_fasta = os.path.join(repository_path, "{}Tr_{}.fasta".format(fasta_file_name, Tr_data_type))
     Val_fasta = os.path.join(repository_path, '{}Va_R_NS_RNS.fasta'.format(fasta_file_name))
@@ -103,37 +110,36 @@ def Deep_Sloop_Model(repository_path=r"C:/Users/Douglas/PycharmProjects/Deep_Slo
         model.add(MaxPooling1D(pool_size=mp_pool_size, strides=mp_strides))
 
     print("Adding BiLSTM Layers...\n")
-    model.add(Bidirectional(CuDNNLSTM(mode1_size, return_sequences=True),
+    model.add(Bidirectional(CuDNNLSTM(lstm_size, return_sequences=True),
                             input_shape=(max_sloop_len, 4)))
 
-    model.add(Bidirectional(CuDNNLSTM(mode1_size, return_sequences=False),
+    model.add(Bidirectional(CuDNNLSTM(lstm_size, return_sequences=False),
                             input_shape=(max_sloop_len, 4)))
 
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(16, activation='relu'))
+    model.add(Dropout(0.5))
+
     model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(optimizer=optimizers.RMSprop(lr=0.001),
-                  loss="binary_crossentropy",
+    optimizer_name = 'Adam'
+    optimizer_fn = optimizers.Adam()
+    loss_fn = 'binary_crossentropy'
+
+
+    model.compile(optimizer=optimizer_fn,
+                  loss=loss_fn,
                   metrics=['accuracy'])
 
     # Print out data pertinent to the I/O of each layer
     print("Model Architecture and I/O:")
     for layer in model.layers:
-        print('Layer: {:>15}   Input Shape: {:<15}   Output Shape: {}'
+        print('Layer: {:>15}   Input Shape: {:<15}   Output Shape: {}\n'
               .format(layer.name, str(layer.input_shape), layer.output_shape))
-    print("")
+
+    # Set the results directory
+    results_dir = os.path.join(repository_path.split('/Data')[0], 'Model_Results')
 
     # Set up your callbacks
-    wts_ext_num = 0
-    wts_returned = False
-    while not wts_returned:
-        weights_name = "{}{}wts_{}_".format(fasta_file_name, Tr_data_type, wts_ext_num) + "{epoch:02d}-{val_acc:.3f}.hdf5"
-        weights_path = repository_path.split('/Datasets')[0] + '/Results/{}'.format(weights_name)
-        if os.path.exists(weights_path):
-            wts_ext_num += 1
-        else:
-            wts_returned = True
+    weights_path = os.path.join(results_dir, 'weights.hdf5')
 
     checkpointing = keras.callbacks.ModelCheckpoint(weights_path,
                                                     monitor='val_loss',
@@ -154,6 +160,12 @@ def Deep_Sloop_Model(repository_path=r"C:/Users/Douglas/PycharmProjects/Deep_Slo
                         verbose=2,
                         callbacks=[early_stopping, checkpointing])
 
+    # Display runtime information
+    end_time = time.time() - start_time
+    print('\nrun time = {:.3f} sec, {:.3f} min, {:.3f} hrs'.format(end_time, end_time / 60.0, end_time / 3600.0))
+
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
     history_dict = history.history
 
     # Extract accuracy and loss data for your training and validation
@@ -162,13 +174,32 @@ def Deep_Sloop_Model(repository_path=r"C:/Users/Douglas/PycharmProjects/Deep_Slo
     val_acc = history_dict['val_acc']
     val_loss = history_dict['val_loss']
     epoch_range = range(1, len(train_acc) + 1)
-    final_val_acc = str(round(float(val_acc[-1]), 3))[2:]
 
     # Report how your model improved over the course of the training session (Looks like data is end of epoch)
     print('\n  acc change: {:.3f} -> {:.3f}'.format(train_acc[0], train_acc[-1]))
     print(' val_acc change: {:.3f} -> {:.3f}'.format(val_acc[0], val_acc[-1]))
     print('    loss change: {:.3f} -> {:.3f}'.format(train_loss[0], train_loss[-1]))
     print('val_loss change: {:.3f} -> {:.3f}'.format(val_loss[0], val_loss[-1]))
+
+    # Compute core model metrics for naming file and model assessment purposes
+    min_loss = min(train_loss)
+    min_loss_idx = train_loss.index(min_loss)
+
+    max_acc = max(train_acc)
+    max_acc_idx = train_acc.index(max_acc)
+
+    min_val_loss = min(val_loss)
+    min_val_loss_idx = val_loss.index(min_val_loss)
+
+    max_val_acc = max(val_acc)
+    max_val_acc_idx = val_acc.index(max_val_acc)
+
+    base_output_filename = 'run_{}_res_{}_{}_{:.3f}_{:.3f}_{:.3f}' \
+        .format(timestamp, custom_expt_name, min_val_loss_idx, min_val_loss, val_acc[min_val_loss_idx], max_val_acc)
+
+    # Select the weights file from your checkpoints that has the best performance, as measured by validation loss
+    new_weights_filename = os.path.join(results_dir, base_output_filename + '.hdf5')
+    os.rename(weights_filename, new_weights_filename)
 
     # Accuracy versus epochs for training and validation
     plt.subplot(1, 2, 1)
@@ -189,25 +220,54 @@ def Deep_Sloop_Model(repository_path=r"C:/Users/Douglas/PycharmProjects/Deep_Slo
     plt.legend()
 
     # Save your image file
-    plt_ext_num = 0
-    plt_made = False
-    while not plt_made:
-        image_name = '{}_{}_{}_fig{}'.format(fasta_file_name, Tr_data_type, final_val_acc, plt_ext_num)
-        images_path = repository_path.split('/Datasets')[0] + '/Results/{}'.format(image_name)
-        if os.path.exists(images_path):
-            plt_ext_num += 1
-        else:
-            plt.savefig(images_path)
-            plt_made = True
+    image_filename = os.path.join(results_dir, base_output_filename + '.pdf')
+    plt.savefig(image_filename)
 
-    # Output your validation data into a CSV for figure generation
-    csv_name = '{}_{}_{}_{}.csv'.format(fasta_file_name, Tr_data_type, final_val_acc, custom_expt_name)
-    csv_path = repository_path.split('/Datasets')[0] + '/Results/{}'.format(csv_name)
-    if not os.path.exists(csv_path):
-        with open(csv_path, 'a+') as f:
-            f.write('Trial,{},Epoch,Validation_Loss,Validation_Accuracy\n'.format(custom_expt_var))
-            for epoch in range(len(epoch_range)):
-                f.write('{},{},{},{},{}\n'.format(1, custom_expt_value, epoch, val_loss[epoch], val_acc[epoch]))
+    # Output the validation data into a CSV for figure generation
+    csv_filename = os.path.join(results_dir, base_output_filename + '.csv')
+    with open(csv_filename, 'a+') as f:
+        f.write('Trial,{},Epoch,Validation_Loss,Validation_Accuracy\n'.format(custom_expt_var))
+        for epoch in range(len(epoch_range)):
+            f.write('{},{},{},{},{}\n'.format(1, custom_expt_value, epoch, val_loss[epoch], val_acc[epoch]))
+
+    # Log run details into a txt summary file
+    log_filename = os.path.join(results_dir, base_output_filename + '.txt')
+    with open(log_filename, 'w') as f:
+        f.write('{}\n'.format(base_output_filename))
+        f.write('\n')
+        f.write('acc     : {:.4f} -> [{}]{:.4f} -> {:.4f}\n'.format(train_acc[0], max_acc_idx, max_acc, train_acc[-1]))
+        f.write(
+            'loss    : {:.4f} -> [{}]{:.4f} -> {:.4f}\n'.format(train_loss[0], min_loss_idx, min_loss, train_loss[-1]))
+        f.write(
+            'val acc : {:.4f} -> [{}]{:.4f} -> {:.4f}\n'.format(val_acc[0], max_val_acc_idx, max_val_acc, val_acc[-1]))
+        f.write('val loss: {:.4f} -> [{}]{:.4f} -> {:.4f}\n'.format(val_loss[0], min_val_loss_idx, min_val_loss,
+                                                                    val_loss[-1]))
+        f.write('\n')
+        f.write('val_acc at min_val_loss_idx = {:.4f}\n'.format(val_acc[min_val_loss_idx]))
+        f.write('val_loss at max_val_acc_idx = {:.4f}\n'.format(val_loss[max_val_acc_idx]))
+        f.write('\n')
+        for layer in model.layers:
+            f.write('Layer: {:>18}   Input Shape: {:<18}   Output Shape: {}\n'
+                    .format(layer.name, str(layer.input_shape), layer.output_shape))
+        f.write('\n')
+        f.write('repository path = {}\n'.format(repository_path))
+        f.write('Tr_data_type = {}\n'.format(Tr_data_type))
+        f.write('Convolve = {}\n'.format(Convolve))
+        if Convolve:
+            f.write('cv_filters = {}\n'.format(cv_filters))
+            f.write('cv_kernel_size = {}\n'.format(cv_kernel_size))
+            f.write('cv_strides = {}\n'.format(cv_strides))
+            f.write('mp_pool_size = {}\n'.format(mp_pool_size))
+            f.write('mp_strides = {}\n'.format(mp_strides))
+
+        f.write('lstm1 size = {}\n'.format(lstm1_size))
+        f.write('lstm2 size = {}\n'.format(lstm1_size))
+        f.write('epochs = {}\n'.format(epochs))
+        f.write('batch size = {}\n'.format(batch_size))
+
+        f.write('\n')
+        f.write('optimizer fn = {}\n'.format(optimizer_name))
+        f.write('loss fn = {}\n'.format(loss_fn))
 
 
 Deep_Sloop_Model()
